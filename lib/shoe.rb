fd8632ece31b0d64da238a6b0c9ecc20f87f5eff
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-08-19 00:05:08 +0300

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,19 @@
 # Make your shoe class here!
+lass Shoe
+  attr_accessor :brand
+  attr_accessor :color
+  attr_accessor :material
+  attr_accessor :condition
+  attr_accessor :cobble
+  attr_accessor :size
+  def initialize(brand)
+    @brand = brand
+    # @cobble = 
+  end
+
+  def cobble
+
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+end 
